f07d501b2fece260e467f1e7d12025a542bcaa28
Kelly Jerrell <kcjerrell@gmail.com>
2021-05-10 20:26:35 -0700

    this commit broke

---
+++ Bridges/KnownDevices.cs
@@ -0,0 +1,72 @@
+﻿using MyLights.Properties;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLights.Bridges
+{
+    public static class KnownDevices
+    {
+        public static string GetName(string id)
+        {
+            var device = GetDevice(id);
+            return device.Name;
+        }
+
+        private static ObservableCollection<DevicePreferences> devices;
+
+        private static DevicePreferences GetDevice(string id)
+        {
+            DevicePreferences device;
+            try
+            {
+                device = devices.Single(d => d.Id == id);
+            }
+            catch (InvalidOperationException)
+            {
+                device = new DevicePreferences();
+                device.Id = id;
+                device.Name = "Unnamed Device";
+                devices.Add(device);
+            }
+
+            return device;
+        }
+
+        internal static void UpdateName(string id, string name)
+        {
+            var dev = GetDevice(id);
+            if (dev.Name != name)
+            {
+                dev.Name = name;
+                SaveDevices();
+            }
+        }
+
+        static KnownDevices()
+        {
+            if (string.IsNullOrEmpty(Settings.Default.DevicePreferences))
+                devices = new ObservableCollection<DevicePreferences>();
+            else
+                devices = JsonConvert.DeserializeObject<ObservableCollection<DevicePreferences>>(Settings.Default.DevicePreferences);
+
+            devices.CollectionChanged += (s,e) => SaveDevices();
+        }
+
+        private static void SaveDevices()
+        {
+            Settings.Default.DevicePreferences = JsonConvert.SerializeObject(devices);
+        }
+
+
+        public class DevicePreferences
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ LightMods/BubblerEffect.cs
@@ -0,0 +1,94 @@
+﻿using MyLights.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyLights.LightMods
+{
+    [MultiLightEffect("Bubbler", "/Resource/Puzzles-256.png")]
+    public class BubblerEffect : ILightEffect
+    {
+        private IList<LightViewModel> lights;
+        private CancellationTokenSource cancelSource;
+        private Task sweepTask;
+
+        public bool IsActive { get; private set; }
+        public List<PluginSetting> Settings { get; }
+
+        const double peakV = 0.9;
+        const double baseV = 0.2;
+        const double totalFadeTime = 1500.0;
+        const int delayInterval = 200;
+
+        public async Task Bubble(CancellationToken token)
+        {
+            foreach (var light in lights)
+            {
+                light.Light.SetMode(Models.LightMode.Color);
+                light.V = baseV;
+            }
+
+            int li = 0;
+            double decay = (peakV - baseV) / totalFadeTime / (delayInterval / 1000.0);
+            int totalProc = (int)Math.Floor(totalFadeTime / delayInterval);
+            int p = totalProc;
+            double range = peakV - baseV;
+
+            while (IsActive)
+            {
+                var light = lights[li].Light;
+                var col = light.Color;
+                double v = p * range + baseV;
+                col = new Models.HSV(col.H, col.S, v);
+                               
+                light.SetColor(col, true);
+
+                p -= 1;
+
+                if (p < 0)
+                {
+                    p = totalProc;
+                    li = (li + 1) % lights.Count;
+                }
+
+                await Task.Delay(delayInterval);
+
+                if (token.IsCancellationRequested)
+                {
+                    IsActive = false;
+                }
+            }
+        }
+
+        public void Attach(IModHost modHost, IList<LightViewModel> lights)
+        {
+            this.lights = lights;
+        }
+
+        public void Suspend()
+        {
+            cancelSource?.Cancel();
+        }
+
+        public void Shutdown()
+        {
+            cancelSource?.Cancel();
+        }
+
+        IEnumerable<PluginSetting> ILightEffect.Settings { get; }
+
+        public void Start()
+        {
+            IsActive = true;
+            cancelSource = new CancellationTokenSource();
+            sweepTask = Bubble(cancelSource.Token).ContinueWith((task) =>
+            {
+                cancelSource.Dispose();
+                cancelSource = null;
+            });
+        }
+    }
+}
